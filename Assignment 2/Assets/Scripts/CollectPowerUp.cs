818a34d19b2063286b8d872c942873c3d9813403
Alek2750 <alek2750@gmail.com>
2019-08-25 21:38:39 +0200

    PowerUP & score done

---
+++ Assignment 2/Assets/Scripts/CollectPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPowerUp : MonoBehaviour
+{
+    
+
+    void OnTriggerEnter(Collider collision)
+    {
+        
+        ScoreCounter.scoreValue += 1;
+        Destroy(gameObject);
+    }
+}
--- Assignment 2/Assets/Scripts/ScoreCounter.cs
+++ Assignment 2/Assets/Scripts/ScoreCounter.cs
@@ -1,40 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour
 {
-    /*[SerializeField]
-    private Text scoreText;
+    
+    public GameObject scoreText;
 
-    private int scoreValue;
+    public static int scoreValue;
 
-    private bool doublePowerUp;
+    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        doublePowerUp = false;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score: " + scoreValue;
+        scoreText.GetComponent<Text>().text = "PowerUP SCORE: " + scoreValue;
     }
-
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.name.Equals("Double"))
-        {
-            doublePowerUp = true;
-            collision.GameObject.SetActive(false);
-        }
-
-        if (collision.name.Equals("PowerUP") && !doublePowerUp)
-            scoreValue += 1;
-
-        if (collision.name.Equals("PowerUP") && doublePowerUp)
-            scoreValue += 2;
-    }*/
 }
