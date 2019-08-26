7e586a84c5ed02de1fb276e7f375922d03142e02
Alek2750 <alek2750@gmail.com>
2019-08-26 19:02:51 +0200

    add time to timer, needs work

---
+++ Assignment 2/Assets/Scripts/AddTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTime : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUpTime")
+        {
+            //add time
+            CountdownTimer.currentTime += 10;
+            Destroy(other.gameObject);
+        }
+    }
+}
