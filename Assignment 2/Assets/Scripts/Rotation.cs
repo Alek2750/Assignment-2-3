31f47239ae955469357ea07882d48642d8356c44
Alek2750 <alek2750@gmail.com>
2019-08-27 18:56:41 +0200

    rotation done

---
+++ Assignment 2/Assets/Scripts/Rotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public GameObject ai;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SelfAround();
+        
+    }
+    void SelfAround()
+    {
+        transform.RotateAround(ai.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
